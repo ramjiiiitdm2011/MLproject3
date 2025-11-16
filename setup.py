024ea51368834e3bf204b9ca419913bd1883beeb
ramjiiiitdm2011 <ramji.iiitdm2011@gmail.com>
2025-11-16 13:42:14 +0530

    Add requirements.txt, setup.py, and __init__.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+HYPEN_E_DOT ='-e .'
+def get_requirements(file_path: str):
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
+setup(
+    name="mlproject3",
+    version="0.0.1",
+    author="RamjiLal",
+    author_email="ramji.iiitdm2011@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
